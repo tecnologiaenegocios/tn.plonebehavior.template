34aa4a19ac1fc7ff52ea0d9ac13df944f1e9754d
rcabralc <rcabralc@gmail.com>
2014-02-20 18:12:04 -0300

    Add a validation to ensure that CSS selector actually works

--- src/tn/plonebehavior/template/html_page_html.py
+++ src/tn/plonebehavior/template/html_page_html.py
@@ -6,8 +6,18 @@ except ImportError:
 
 if HAS_HTML_PAGE:
     from five import grok
+    from tn.plonebehavior.template import _
+    from tn.plonebehavior.template import ITemplateConfiguration
     from tn.plonebehavior.template import interfaces
     from tn.plonebehavior.template.html import ContextlessHTML
+    from z3c.form import validator
+
+    import collections
+    import lxml.cssselect
+    import lxml.html
+    import zope.interface
+
+    isiterable = lambda o: isinstance(o, collections.Iterable)
 
     class HTMLPageHTML(grok.Adapter):
         grok.context(html_page.IHTMLPageSchema)
@@ -19,3 +29,24 @@ if HAS_HTML_PAGE:
             base_url = self.context.absolute_url()
             return unicode(self.contextless_factory(base_url,
                                                     self.context.html))
+
+    class CSSSelectorValidator(validator.SimpleFieldValidator):
+        def validate(self, value):
+            super(CSSSelectorValidator, self).validate(value)
+
+            tree = lxml.html.document_fromstring(self.context.html)
+            xpath = lxml.cssselect.CSSSelector(value).path
+            selection = tree.xpath(xpath)
+            if not isiterable(selection) or len(selection) != 1:
+                raise zope.interface.Invalid(_(
+                    "Expression doesn't select a single element "
+                    "in the HTML page."
+                ))
+
+    validator.WidgetValidatorDiscriminators(
+        CSSSelectorValidator,
+        context=html_page.IHTMLPageSchema,
+        field=ITemplateConfiguration['css']
+    )
+
+    grok.global_adapter(CSSSelectorValidator)
