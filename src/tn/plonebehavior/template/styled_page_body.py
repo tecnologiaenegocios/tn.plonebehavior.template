7adf2f02c806e2ef131f1d440b092064e7c3f5ca
rcabralc <rcabralc@gmail.com>
2014-02-20 20:41:41 -0300

    Use transformation provided directly from styled page objects

--- src/tn/plonebehavior/template/styled_page_body.py
+++ src/tn/plonebehavior/template/styled_page_body.py
@@ -7,15 +7,10 @@ except ImportError:
 if HAS_STYLED_PAGE:
     from five import grok
     from tn.plonebehavior.template import interfaces
-    from tn.plonebehavior.template.html import ContextlessHTML
 
     class StyledPageBody(grok.Adapter):
         grok.context(styled_page.IStyledPageSchema)
         grok.implements(interfaces.IHTMLBody)
 
-        contextless_factory = ContextlessHTML
-
         def __unicode__(self):
-            base_url = self.context.absolute_url()
-            return unicode(self.contextless_factory(base_url,
-                                                    self.context.body.output))
+            return self.context.transform_body()
--- src/tn/plonebehavior/template/tests/test_styled_page_body.py
+++ src/tn/plonebehavior/template/tests/test_styled_page_body.py
@@ -5,24 +5,6 @@ import unittest
 
 
 class TestStyledPageBody(unittest.TestCase):
-
-    def setUp(self):
-        self.old_factory = StyledPageBody.contextless_factory
-
-        class ContextlessFactory(object):
-            def __init__(self, url, html):
-                self.url, self.html = url, html
-            def __unicode__(self):
-                return u', '.join((self.url, self.html))
-        StyledPageBody.contextless_factory = ContextlessFactory
-
-    def tearDown(self):
-        StyledPageBody.contextless_factory = self.old_factory
-
     def test_returns_html_attribute_from_original_context(self):
-        context = stubydoo.double(
-            body=stubydoo.double(output=u'original HTML'),
-            absolute_url=lambda self:u'URL'
-        )
-        self.assertEquals(unicode(StyledPageBody(context)),
-                          u'URL, original HTML')
+        context = stubydoo.double(transform_body=lambda self: u'body')
+        self.assertEquals(unicode(StyledPageBody(context)), u'body')
